a1f2f21e08643113582800c3075735529af41487
Matt Deitke <mdeitke23@gmail.com>
2022-05-30 01:00:59 -0700

    update tests

--- tests/load_test.py
+++ tests/load_test.py
@@ -3,6 +3,14 @@ from github import GithubException
 import prior
 
 
+def test_hello():
+    print("Hello, World!")
+
+
+# def test_dataset():
+# dataset = prior.load_dataset("procthor-dataset")
+
+
 def test_failed_dataset():
     """Test that a non-existant dataset raises an exception."""
     try:
