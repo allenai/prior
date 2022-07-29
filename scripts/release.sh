c1a2b0963fc3d747f098d4a4e9c2103bb5c4d365
lucaw <lucaw@allenai.org>
2022-07-29 15:19:43 -0700

    Fixing dangerous release script.

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,14 +2,19 @@
 
 set -e
 
+if [[ $(git status -suno) ]]; then
+    echo "There are uncommitted changes on this git repository, please commit these and try again."
+    exit 1
+fi
+
 TAG=$(python -c 'from prior.version import VERSION; print("v" + VERSION)')
 
 read -p "Creating new release for $TAG. Do you want to continue? [Y/n] " prompt
 
 if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]]; then
     python scripts/prepare_changelog.py
-    git add -A
-    git commit -m "Bump version to $TAG for release" || true && git push
+    git add CHANGELOG.md
+    (git commit -m "$TAG release" || true) && git push
     echo "Creating new git tag $TAG"
     git tag "$TAG" -m "$TAG"
     git push --tags
